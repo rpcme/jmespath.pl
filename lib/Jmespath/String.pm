f2a6e55c338563646fd36a5c56a6bd8763bdf1ef
Rich Elberger <rich@richelberger.com>
2016-12-28 11:01:11 -0500

    hacky module to force a number to behave like a string ... likely very buggy but used to pass compliance tests that force us to perform operations on typed values

---
+++ lib/Jmespath/String.pm
@@ -0,0 +1,17 @@
+package Jmespath::String;
+use strict;
+use warnings;
+use Data::Dumper;
+use overload (
+              "0+"     => sub { "\"${$_[0]}\""; },
+              "++"     => sub { $_[0] = ${$_[0]} + 1 },
+              "--"     => sub { $_[0] = ${$_[0]} - 1 },
+              '""'     => sub { ${$_[0]}},
+              "."      => sub { ${$_[0]}},
+              "eq"     => sub { ${$_[0]} },
+              fallback => sub { ${$_[0]} },
+);
+
+sub TO_JSON { return '' . shift; }
+
+1;
